e45038537df71cf847a287dbb08b7401a03b65c2
fwh <15279279713@163.com>
2021-03-12 10:26:45 +0800

    浼樺寲鎷栨嫿

--- src/pages/trag/HorizontalDemo/index.tsx
+++ src/pages/trag/HorizontalDemo/index.tsx
@@ -4,12 +4,16 @@ import arrayMove from 'array-move';
 import './index.less';
 const HorizontalDemo = () => {
   const items = [
-    'https://pics7.baidu.com/feed/0ff41bd5ad6eddc43738746669239af550663354.jpeg?token=cc0e1fc3ef641de5a8fc9e17586ba3e6',
-    'https://pics2.baidu.com/feed/b999a9014c086e060045786a67e450fc0bd1cb12.jpeg?token=b66c3f04ad0b57b94f9b2b2f7d830cf9&s=BC0FB0541400255D46CA1B930300808Fs',
-    'https://pics1.baidu.com/feed/c8ea15ce36d3d539038ac4464132c558342ab03a.jpeg?token=9894c94fb0a0e15c30c1cf5eb8bbd3a0&s=5202B20846633AB0469D048303007022',
+    'https://img.gujiwuqing.top/093044-1551835844245d.jpg',
+    'https://img.gujiwuqing.top/201425-1549541665f301.jpg',
+    'https://img.gujiwuqing.top/201711-1554121031da2d.jpg',
   ];
   const [list, setList] = useState(items);
-  const SortableItem = SortableElement(({ value }: any) => <img src={value} />);
+  const SortableItem = SortableElement(({ value }: any) => (
+    <li style={{ listStyle: 'none' }}>
+      <img src={value} style={{ width: '120px' }} />
+    </li>
+  ));
 
   const Sortable = SortableContainer(({ children }: any) => {
     return <ul>{children}</ul>;
--- src/pages/trag/VerticalDemo/index.tsx
+++ src/pages/trag/VerticalDemo/index.tsx
@@ -1,21 +1,27 @@
 import React, { useState } from 'react';
 import { SortableContainer, SortableElement } from 'react-sortable-hoc';
 import arrayMove from 'array-move';
-
+import './index.less';
 const SortableItem = SortableElement(({ value }: any) => (
-  <li tabIndex={0}>{value}</li>
+  <li tabIndex={0} style={{ cursor: 'move' }}>
+    {value}
+  </li>
 ));
 
 const SortableList = SortableContainer(({ items }: any) => {
   return (
     <ul>
       {items.map((value: any, index: number) => (
-        <SortableItem key={`item-${value}`} index={index} value={value} />
+        <SortableItem key={value} index={index} value={value} />
       ))}
     </ul>
   );
 });
 
+// const onSortStart = (node: any) => {
+//   console.log('node', node);
+//   node.helper.className = 'move';
+// };
 const SortableComponent = () => {
   const items = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6'];
   const [list, setList] = useState(items);
